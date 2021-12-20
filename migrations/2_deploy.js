2136f633d9267ac60319f4fe0b28ece0c75f7231
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2021-12-20 19:05:14 +0530

    Completed Smart Contract

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,13 +2,20 @@ const Token = artifacts.require("Token");
 const dBank = artifacts.require("dBank");
 
 module.exports = async function(deployer) {
-	//deploy Token
 
+	await deployer.deploy(Token);
+	//deploy Token
+	await deployer.deploy(Token);
 	//assign token into variable to get it's address
-	
+	const tkn = await Token.deployed();
+
 	//pass token address for dBank contract(for future minting)
+	await deployer.deploy(tkn.address);
 
 	//assign dBank contract into variable to get it's address
+	const dBank = await dBank.deployed();
 
 	//change token's owner/minter from deployer to dBank
+	await tkn.passMinterRole(dBank.address);
+
 };
--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -6,42 +6,76 @@ import "./Token.sol";
 contract dBank {
 
   //assign Token contract to variable
-
+Token private token;
   //add mappings
+  mapping(address => uint) public etherBalanceOf;
+  mapping(address => uint) public depositStart;
+  mapping(address => bool) public isDeposited;
 
   //add events
 
+  event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Withdraw(address indexed user, uint etherAmount, uint depositTime, uint interest);
+
   //pass as constructor argument deployed Token contract
-  constructor() public {
+  constructor(Token _tkn) public {
     //assign token deployed contract to variable
+    token = _tkn;
   }
 
   function deposit() payable public {
+
+    require(isDeposited[msg.sender] == false, 'Error, deposit already active');
+    require(msg.value>=1e16, 'Error, deposit must be >= 0.01 ETH');
     //check if msg.sender didn't already deposited funds
     //check if msg.value is >= than 0.01 ETH
+    etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
+    depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+
+    isDeposited[msg.sender] = true;
 
     //increase msg.sender ether deposit balance
     //start msg.sender hodling time
 
     //set msg.sender deposit status to true
     //emit Deposit event
+
+    emit Deposit(msg.sender, msg.value, block.timestamp);
   }
 
   function withdraw() public {
+    require(isDeposited[msg.sender] == true, 'Error, No money Deposited');
+    uint userBalance = etherBalanceOf[msg.sender];
     //check if msg.sender deposit status is true
     //assign msg.sender ether deposit balance to variable for event
 
     //check user's hodl time
 
+    uint depositTime = block.timestamp - depositStart[msg.sender];
+
     //calc interest per second
+
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender]);
+    uint interest = interestPerSecond * depositTime;
+
     //calc accrued interest
 
     //send eth to user
+
+    msg.sender.transfer(userBalance);
+
+    token.mint(msg.sender, interest);
     //send interest in tokens to user
 
     //reset depositer data
+    depositStart[msg.sender] = 0;
+    etherBalanceOf[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
 
     //emit event
+
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
+
   }
 
   function borrow() payable public {
